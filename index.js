9b14c50346e6ec429baa9bbbda5299991c3b1e1b
minhnguyen1408 <acc.tr100@gmail.com>
2023-07-20 12:17:49 -0400

    mac

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i < numberOfDrumButtons; i++) {
 
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-
+$(".drum").each(function(){
+  this.addEventListener("click", function(){
     var buttonInnerHTML = this.innerHTML;
-
     makeSound(buttonInnerHTML);
-
     buttonAnimation(buttonInnerHTML);
+  })
+})
 
-  });
-
-}
-
-document.addEventListener("keypress", function(event) {
-
+$(document).on("keypress", function(event) {
   makeSound(event.key);
-
   buttonAnimation(event.key);
-
 });
 
 
@@ -70,7 +61,7 @@ function makeSound(key) {
 
 function buttonAnimation(currentKey) {
 
-  var activeButton = document.querySelector("." + currentKey);
+  var activeButton = $("." + currentKey);
 
   activeButton.classList.add("pressed");
 
